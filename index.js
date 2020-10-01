220e3efd59697cecd52868c730dae769f671f65e
Clement Garcin <69050287+gclement28@users.noreply.github.com>
2020-10-01 10:29:32 +0200

    j'ai ajouté un index et un gitignore

---
+++ index.js
@@ -0,0 +1,7 @@
+const chalk = require('chalk');
+
+const myColleagues =["nathan", "christian", "karen", "caroline"];
+console.log(chalk.blue(myColleagues[0]));
+console.log(chalk.red(myColleagues[1]));
+console.log(chalk.green(myColleagues[2]));
+console.log(chalk.yellow(myColleagues[3]));
